bb400f8ed15f4e564f93bb6867cfcb7bc3d29e45
sriramsravishta <63202730+sriramsravishta@users.noreply.github.com>
2025-10-08 17:10:47 +0530

    Updated MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -125,7 +125,7 @@ export default function MainPage() {
 
         <div className="space-y-8">
           <section>
-            <h2 className="text-xl font-semibold text-gray-900 mb-4 mt-6">
+            <h2 className="text-xl font-semibold text-gray-900 mb-4 mt-10">
               Today's Pre-consult Completed
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-3">

70eb6909bf3755a0e5919ee6e410ef9fa0cbffb9
sriramsravishta <63202730+sriramsravishta@users.noreply.github.com>
2025-10-08 17:06:09 +0530

    Updated MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -198,7 +198,7 @@ export default function MainPage() {
                     )}
                       
                     </div>
-                    <div className="text-xs text-gray-500 bg-gray-100 px-2 py-1 rounded">
+                    <div className="text-sm text-gray-500">
                         {patient.age}y, {patient.gender}
                       </div>
                     

737b380aa02bc6af83ac79b6e9d40c9edfef4169
sriramsravishta <63202730+sriramsravishta@users.noreply.github.com>
2026-01-19 14:15:58 +0530

    Updated MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -230,7 +230,7 @@ export default function MainPage() {
         <div className="space-y-8">
           <section>
             <h2 className="text-lg font-semibold text-gray-900 mb-4 mt-10">
-              Today's Patients
+              Today's Patient Que
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-3">
               {loading ? (

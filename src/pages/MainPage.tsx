b27093e74d94860cba704e2ceeaf30816d113387
sriramsravishta <63202730+sriramsravishta@users.noreply.github.com>
2026-02-03 20:07:33 +0530

    Updated MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -81,6 +81,9 @@ export default function MainPage() {
   const filteredTodaysAppointments = todaysAppointments.filter(appointment =>
     appointment.patients?.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
+  const pendingTodaysAppointments = filteredTodaysAppointments.filter(a => a.completed !== true);
+const completedTodaysAppointments = filteredTodaysAppointments.filter(a => a.completed === true);
+
 
   const filteredAllPatients = allPatients.filter(p =>
     p.name.toLowerCase().includes(searchQuery.toLowerCase())

9b2f7663d64010a9e19486c5d412a01ecabd23f1
sriramsravishta <63202730+sriramsravishta@users.noreply.github.com>
2026-02-21 11:06:43 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import Login from './pages/Login';
 import MainPage from './pages/MainPage';

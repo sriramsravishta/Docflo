3d16daf1204a632033e488b074f1dd7b552d4818
sriramsravishta <63202730+sriramsravishta@users.noreply.github.com>
2026-02-26 10:45:13 +0530

    Updated MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -243,7 +243,7 @@ const completedTodaysAppointments = filteredTodaysAppointments.filter((a) => a.c
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
             <input
               type="text"
-              placeholder="Search patients by name..."
+              placeholder="Search by name..."
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
               className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#024CDB] focus:border-transparent"

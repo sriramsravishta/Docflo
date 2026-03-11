9323a699d7633b9f640c97081220e774c106a630
sriramsravishta <63202730+sriramsravishta@users.noreply.github.com>
2026-03-11 18:23:30 +0530

    Updated SummaryDetail.tsx

--- src/pages/SummaryDetail.tsx
+++ src/pages/SummaryDetail.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { ChevronRight, Download } from 'lucide-react';
 import { CreditCard as Edit } from 'lucide-react';

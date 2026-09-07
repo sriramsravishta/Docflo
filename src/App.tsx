ff048044805ba1ecf13c8023636bd657ed07ba1d
sriramsravishta <63202730+sriramsravishta@users.noreply.github.com>
2026-09-07 10:51:51 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import SummaryDetail from './pages/SummaryDetail';
 import FavouritesPage from './pages/FavouritesPage';
 import ResetPassword from './pages/ResetPassword';
 import ProtectedRoute from './components/ProtectedRoute';
+import PrescriptionPage from './pages/PrescriptionPage';
 
 // Pharmacy pages
 import PharmacyDashboard from './pages/pharmacy/PharmacyDashboard';
